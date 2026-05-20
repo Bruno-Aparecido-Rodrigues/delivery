a643d6fdd50307b3f85f5adbca65880daa13b10f
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2026-05-20 17:29:46 -0300

    front end simples

---
+++ src/app/index.tsx
@@ -0,0 +1,45 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.formWrapper}>
+                <Text style={styles.label}>Texto</Text>
+                <TextInput style={styles.input} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#444',
+        flex: 1,
+        padding: 32,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    formWrapper: {
+        gap: 8,
+        marginBottom: 80,
+        width: '100%',
+        maxWidth: 320,
+    },
+    label: {
+        color: '#FFF',
+        fontSize: 14,
+        fontWeight: '600',
+        letterSpacing: 0.5,
+        textAlign: 'center',
+    },
+    input: {
+        backgroundColor: '#555',
+        borderRadius: 8,
+        color: '#FFF',
+        fontSize: 16,
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        borderWidth: 1,
+        borderColor: '#666',
+    },
+});
